380cfde58dc94700790d04d23dc0ce2318c1afd5
Izarley Rodrigues <izarley.vac@hotmail.com>
2023-01-09 15:12:06 -0300

    createLayout.att

---
+++ SQLite with RecyclerView/app/src/main/java/br/com/flap/listoftasks/AddTaskActivity.java
@@ -0,0 +1,14 @@
+package br.com.flap.listoftasks;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddTaskActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_task);
+    }
+}
--- SQLite with RecyclerView/app/src/main/java/br/com/flap/listoftasks/MainActivity.java
+++ SQLite with RecyclerView/app/src/main/java/br/com/flap/listoftasks/MainActivity.java
@@ -1,5 +1,6 @@
 package br.com.flap.listoftasks;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.snackbar.Snackbar;
@@ -39,8 +40,8 @@ public class MainActivity extends AppCompatActivity {
         binding.fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Intent intent = new Intent(getApplicationContext(), AddTaskActivity.class);
+                startActivity(intent);
             }
         });
     }

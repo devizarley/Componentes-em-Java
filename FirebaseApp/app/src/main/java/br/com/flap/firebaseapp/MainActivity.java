9aa6ed796472b9ff84cf7baec8b9c9411e1ba37e
Izarley Rodrigues <izarley.vac@hotmail.com>
2023-01-10 22:10:27 -0300

    att

--- FirebaseApp/app/src/main/java/br/com/flap/firebaseapp/MainActivity.java
+++ FirebaseApp/app/src/main/java/br/com/flap/firebaseapp/MainActivity.java
@@ -3,10 +3,19 @@ package br.com.flap.firebaseapp;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
@@ -14,26 +23,149 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.net.URI;
+import java.util.UUID;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ImageView imagePhoto;
+    private Button buttonUpload;
+
+    /*
     private DatabaseReference reference = FirebaseDatabase.getInstance().getReference();
     private FirebaseAuth auth = FirebaseAuth.getInstance();
+    */
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        DatabaseReference users = reference.child( "usuarios" );
+        /*Fazendo upload de uma foto*/
 
-        Usuario user = new Usuario();
-        user.setNome("Izarley");
-        user.setSobrenome("Rodrigues");
-        user.setIdade(35);
+        imagePhoto = findViewById(R.id.imagePhoto);
+        buttonUpload = findViewById(R.id.buttonUpload);
 
-        users.push().setValue(user);
+        buttonUpload.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                //Configura para imagem ser salva em memoria
+                imagePhoto.setDrawingCacheEnabled(true);
+                imagePhoto.buildDrawingCache();
+
+                //Recupera bitmap da imagem (image a ser carregada)
+                Bitmap bitmap = imagePhoto.getDrawingCache();
+
+                //Comprimo bitmap para um formato png/jpeg
+                ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                bitmap.compress(Bitmap.CompressFormat.JPEG, 75,baos );
+
+                //Converte o baos para pixel brutos em uma matriz de bytes
+                //(dados da imagem)
+                byte[] dadosImagem = baos.toByteArray();
+
+                //Define nós para o storage
+                StorageReference storageReference = FirebaseStorage.getInstance().getReference();
+                StorageReference imagens = storageReference.child("imagens");
+
+                /*Baixar uma imagem pela referencia
+
+                StorageReference imagemRef = imagens.child("celular.jpeg");
+
+                Glide.with(MainActivity.this)
+                        .load(imagemRef)
+                        .into(imagePhoto);
+                */
+                /*Apagar uma imagem pela referencia
+                StorageReference imagemRef = imagens.child("celular.jpeg");
+
+                imagemRef.delete().addOnFailureListener(MainActivity.this, new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(MainActivity.this, "Erro ao deletar a imagem", Toast.LENGTH_SHORT).show();
+                    }
+                }).addOnSuccessListener(MainActivity.this, new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void unused) {
+                        Toast.makeText(MainActivity.this, "Sucesso ao deletar", Toast.LENGTH_SHORT).show();
+                    }
+                });*/
+
+                /*Nome da imagem
+                String nomeArquivo = UUID.randomUUID().toString();
+                StorageReference imagemRef = imagens.child("celular.jpeg");
+
+
+                //Retorna objeto que irá controlar o upload
+                UploadTask uploadTask = imagemRef.putBytes(dadosImagem);
+
+                uploadTask.addOnFailureListener(MainActivity.this, new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(MainActivity.this, "Upload da imagem falhou: " + e.getMessage().toString(), Toast.LENGTH_SHORT).show();
+                    }
+                }).addOnSuccessListener(MainActivity.this, new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                        imagemRef.getDownloadUrl().addOnCompleteListener(new OnCompleteListener<Uri>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Uri> task) {
+                                Uri url = task.getResult();
+                                Toast.makeText(MainActivity.this, "Sucesso", Toast.LENGTH_SHORT).show();
+                            }
+                        });
+                    }
+                });
+                */
+            }
+        });
+
+
+        /*Recuperar dados com filtros*/
+
+        //DatabaseReference users = reference.child( "usuarios" );
+        //DatabaseReference userSearch = users.child("-NLSlH4ag8H0fjSp3XrM");
+
+        //Query UserSearch = users.orderByChild("nome").equalTo("Izarley");
+        //Query UserSearch = users.orderByKey().limitToFirst(3);
+        //Query UserSearch = users.orderByKey().limitToLast(2);
+
+        /*Maior ou igual (>=)*/
+        //Query UserSearch = users.orderByChild("idade").startAt(18);
+
+        /*Maior ou igual (<=)*/
+        //Query UserSearch = users.orderByChild("idade").endAt(18);
+
+        /*Entre dois valores*/
+        //Query UserSearch = users.orderByChild("idade").startAt(18).endAt(40);
+
+        /*Filtrar palavras*/
+        //Query UserSearch = users.orderByChild("nome").startAt("J").endAt("J" + "\uf8ff" );
+
+        /*
+        UserSearch.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                //Usuario dataUser = snapshot.getValue(Usuario.class);
+                //Log.i("Data user:", " nome: " + dataUser.getNome());
+                Log.i("Data user:", snapshot.getValue().toString());
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+         });*/
 
 
         /*Deslogar usuario

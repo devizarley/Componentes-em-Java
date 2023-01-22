1031854456026490fa0da1a6f71be288fc455fe6
Izarley Rodrigues <izarley.vac@hotmail.com>
2023-01-22 20:20:36 -0300

    att

--- FloatingActionMenu/app/src/main/java/izarleydev/com/floatingactionmenu/MainActivity.java
+++ FloatingActionMenu/app/src/main/java/izarleydev/com/floatingactionmenu/MainActivity.java
@@ -30,11 +30,6 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        setSupportActionBar(binding.toolbar);
-
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
-        appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
 
         binding.fab.setOnClickListener(new View.OnClickListener() {
             @Override

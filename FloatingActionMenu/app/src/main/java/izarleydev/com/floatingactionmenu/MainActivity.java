78e43994e99847280d832303c739218d0fb1b495
Izarley Rodrigues <izarley.vac@hotmail.com>
2023-01-22 20:46:50 -0300

    att

--- FloatingActionMenu/app/src/main/java/izarleydev/com/floatingactionmenu/MainActivity.java
+++ FloatingActionMenu/app/src/main/java/izarleydev/com/floatingactionmenu/MainActivity.java
@@ -38,6 +38,13 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+        binding.fab2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
     }
 
     @Override
